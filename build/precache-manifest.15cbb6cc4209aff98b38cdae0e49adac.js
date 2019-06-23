d2a28f01fefe1a523a63674c15778122357cdda1
samuli <shulkkonen@luukku.com>
2019-06-23 23:57:43 +0300

    osa3.1

---
+++ build/precache-manifest.15cbb6cc4209aff98b38cdae0e49adac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17741177725490733e0649c6a798d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9410c601baf5a6463aaf",
+    "url": "/static/css/main.01297af9.chunk.css"
+  },
+  {
+    "revision": "d538652694bd439d49af",
+    "url": "/static/js/2.f2f882f0.chunk.js"
+  },
+  {
+    "revision": "9410c601baf5a6463aaf",
+    "url": "/static/js/main.f7b652d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
