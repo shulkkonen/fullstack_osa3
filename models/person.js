3ce96f426f2caeca0611640bc1e56eb1b772c96d
samuli <shulkkonen@luukku.com>
2019-06-24 00:04:25 +0300

    idk

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const url = process.env.MONGODB_URI;
+const url = "mongodb+srv://fullstack:Cocacola89@cluster0-bbkx4.mongodb.net/phoneBook?retryWrites=true&w=majority";
 
 console.log("connecting to", url);
 
